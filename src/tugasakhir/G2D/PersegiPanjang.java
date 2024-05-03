c1330eb6217ccb29f53dcf549c6fe9484ecf39d6
RafiqIkhwan <90625702+RafiqIkhwan@users.noreply.github.com>
2024-05-03 17:57:39 +0700

    Update PersegiPanjang.java
    Menambah baris code untuk menghitung luas persegi panjang

--- src/tugasakhir/G2D/PersegiPanjang.java
+++ src/tugasakhir/G2D/PersegiPanjang.java
@@ -13,9 +13,28 @@ import tugasakhir.Benda2D;
  */
 public class PersegiPanjang implements Benda2D {
 
+    private double panjang;
+    private double lebar;
+    private double luas;
+
+    public PersegiPanjang(double panjang, double lebar){
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+    public double getPanjang(){
+        return panjang;
+    }
+    public double getLebar(){
+        return lebar
+    }
+    public double getLuas(){
+        return luas;
+    }
+    
     @Override
     public double menghitungLuas() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        luas = panjang * lebar;
+        return luas;
     }
     
 }
